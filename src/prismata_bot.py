845bd563d872bbb6c408f6dc79447ddbbdd8e76b
Wiwiweb <wiwiweb2@gmail.com>
2018-04-11 15:40:04 -0700

    Improve unit query to name matching

--- src/prismata_bot.py
+++ src/prismata_bot.py
@@ -7,6 +7,16 @@ import irc.bot
 from globals import config, log
 
 tooltips = json.load(open(config['Files']['unit_tooltips']))
+tooltip_key_to_name = {}  # Dictionary of lowercase search terms to actual unit name
+for tooltip_dict_name in tooltips:
+    tooltip_name_lower = tooltip_dict_name.lower()
+    tooltip_key_to_name[tooltip_dict_name] = tooltip_dict_name
+    name_parts = tooltip_name_lower.split(' ')
+    if len(name_parts) >= 2:
+        for name_part in name_parts:
+            if name_part not in tooltip_key_to_name:
+                tooltip_key_to_name[name_part] = tooltip_dict_name
+
 
 class PrismataBot(irc.bot.SingleServerIRCBot):
     def __init__(self, channel, nickname, server, port, password):
@@ -31,11 +41,12 @@ class PrismataBot(irc.bot.SingleServerIRCBot):
 
     def answer_command(self, connection, query, channel):
         log.info('Answering !unit {} in channel {}'.format(query, channel))
-        tooltip_key = get_close_matches(query, tooltips.keys(), 1, 0.3)
+        tooltip_key = get_close_matches(query, tooltip_key_to_name.keys(), 1, 0.3)
         if tooltip_key:
             tooltip_key = tooltip_key[0]
             log.debug('Closest match: {} with {}'.format(tooltip_key, SequenceMatcher(None, query, tooltip_key).ratio()))
-            connection.privmsg(self.channel, '{}: {}'.format(tooltip_key, tooltips[tooltip_key]))
+            tooltip_name = tooltip_key_to_name[tooltip_key]
+            connection.privmsg(self.channel, '{}: {}'.format(tooltip_name, tooltips[tooltip_name]))
         else:
             log.debug('Not found')
             connection.privmsg(self.channel, "Couldn't find a unit for {}!".format(query))
