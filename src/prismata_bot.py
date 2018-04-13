dd1576e1f91a1fbd021e69e6b18ab892aecdfce4
Wiwiweb <wiwiweb2@gmail.com>
2018-04-12 23:35:46 -0700

    Prismata command fixes

--- src/prismata_bot.py
+++ src/prismata_bot.py
@@ -20,7 +20,7 @@ for tooltip_dict_name in tooltips:
 custom_tooltips = json.load(open(config['Files']['custom_tooltip_matches']))
 tooltip_key_to_name = {**tooltip_key_to_name, **custom_tooltips}  # Merge both dictionaries
 
-prismata_responses = json.load(open(config['Files']['unit_tooltips']))
+prismata_responses = json.load(open(config['Files']['prismata_responses']))
 
 
 class PrismataBot(irc.bot.SingleServerIRCBot):
@@ -38,24 +38,28 @@ class PrismataBot(irc.bot.SingleServerIRCBot):
 
     def on_pubmsg(self, connection, event):
         text = event.arguments[0]
-        msg = text.split(' ', 1)
+        text_split = text.split(' ', 1)
 
         username = ''
         for tag in event.tags:  # Iterating through to find the key because this gives us a list instead of a dict...
             if tag['key'] == 'display-name':
                 username = tag['value']
 
-        if msg[0].startswith(('!prismata', '!unit', '@PrismataBot')):
-            log.info('Answering {} in channel {} from user {}'.format(text, self.channel, username))
+        if text.startswith(('!prismata', '!unit', '@PrismataBot')):
+            log.info('Answering "{}" in channel {} from user {}'.format(text, self.channel, username))
 
-        if msg[0].startswith('!prismata'):
-            self.answer_unit_command(msg[1])
-        elif msg[0].startswith('!unit'):
-            if len(msg) == 2 and len(msg[1]) > 0:
-                self.answer_unit_command(msg[1])
+        if text_split[0] == '!prismata':
+            if len(text_split) == 1 or text_split[1] == '':
+                query = 'prismata'
+            else:
+                query = text_split[1]
+            self.answer_prismata_command(query)
+        elif text_split[0] == '!unit':
+            if len(text_split) == 2 and len(text_split[1]) > 0:
+                self.answer_unit_command(text_split[1])
             else:
                 self.chat('You need to type a unit name FailFish')
-        elif msg[0].startswith('@PrismataBot'):
+        elif text_split[0] == '@PrismataBot':
             self.answer_hello_command(username)
 
     def on_disconnect(self, connection, event):
@@ -77,8 +81,6 @@ class PrismataBot(irc.bot.SingleServerIRCBot):
             self.chat("Couldn't find a unit for {} NotLikeThis".format(query))
 
     def answer_prismata_command(self, query):
-        if query is None or query == '':
-            query = 'prismata'
         tooltip_key = get_close_matches(query, prismata_responses.keys(), 1, 0.5)
         if tooltip_key:
             tooltip_key = tooltip_key[0]
@@ -88,7 +90,7 @@ class PrismataBot(irc.bot.SingleServerIRCBot):
             self.chat(prismata_responses[tooltip_key])
         else:
             log.debug('Not found')
-            query_list = prismata_responses.keys().join(', ')
+            query_list = ', '.join(prismata_responses.keys())
             self.chat("I don't have anything to say about that FrankerZ I can talk about: {}".format(query_list))
 
     def answer_hello_command(self, username):
