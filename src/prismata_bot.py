16b0060eb107819f194024117728ce52bc0bf818
Wiwiweb <wiwiweb2@gmail.com>
2018-04-12 20:27:58 -0700

    Improve tooltip matching

--- src/prismata_bot.py
+++ src/prismata_bot.py
@@ -10,12 +10,15 @@ tooltips = json.load(open(config['Files']['unit_tooltips']))
 tooltip_key_to_name = {}  # Dictionary of lowercase search terms to actual unit name
 for tooltip_dict_name in tooltips:
     tooltip_name_lower = tooltip_dict_name.lower()
-    tooltip_key_to_name[tooltip_dict_name] = tooltip_dict_name
+    tooltip_key_to_name[tooltip_name_lower] = tooltip_dict_name
     name_parts = tooltip_name_lower.split(' ')
     if len(name_parts) >= 2:
         for name_part in name_parts:
             if name_part not in tooltip_key_to_name:
                 tooltip_key_to_name[name_part] = tooltip_dict_name
+
+custom_tooltips = json.load(open(config['Files']['custom_tooltip_matches']))
+tooltip_key_to_name = {**tooltip_key_to_name, **custom_tooltips}  # Merge both dictionaries
 
 
 class PrismataBot(irc.bot.SingleServerIRCBot):
@@ -42,11 +45,13 @@ class PrismataBot(irc.bot.SingleServerIRCBot):
 
     def answer_command(self, connection, query, channel):
         log.info('Answering !unit {} in channel {}'.format(query, channel))
-        tooltip_key = get_close_matches(query, tooltip_key_to_name.keys(), 1, 0.3)
+        tooltip_key = get_close_matches(query, tooltip_key_to_name.keys(), 1, 0.4)
         if tooltip_key:
             tooltip_key = tooltip_key[0]
-            log.debug('Closest match: {} with {}'.format(tooltip_key, SequenceMatcher(None, query, tooltip_key).ratio()))
             tooltip_name = tooltip_key_to_name[tooltip_key]
+            log.debug('Closest match: {}->{}->{} with {}'
+                      .format(query, tooltip_key, tooltip_name,
+                              round(SequenceMatcher(None, query, tooltip_key).ratio(), 2)))
             connection.privmsg(self.channel, '{}: {}'.format(tooltip_name, tooltips[tooltip_name]))
         else:
             log.debug('Not found')
